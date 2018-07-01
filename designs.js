2fc71e043ba56ed1d70907fef8c46d1decc7e54e
daniel nuwa <daniel.nuwa@gmail.com>
2018-07-01 14:37:02 +0300

    added js functionality to designs.js

--- designs.js
+++ designs.js
@@ -2,9 +2,40 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+(function() {
+  'use strict';
 
-function makeGrid() {
+  // set global variables
+  const $colorPicker = document.getElementById("colorPicker");
+  const $sizePicker = document.getElementById("sizePicker");
+  const $table = document.getElementById("pixel_canvas");
 
-// Your code goes here!
+  // add listener to select grid size
+  $sizePicker.addEventListener('submit', function() {
+    // prevent page refresh on submit
+    event.preventDefault();
 
-}
+    // get input data and draw grid
+    let width = document.getElementById("input_width").value;
+    let height = document.getElementById("input_height").value;
+    makeGrid(width, height);
+  })
+
+  function makeGrid(width, height) {
+    $table.innerHTML = '';
+    for (let row = 0; row < width; row++) {
+      let newRow = $table.insertRow();
+        for (let cell= 0; cell < height; cell++ ) {
+          // add new cell with listener to change color
+          let newCell = newRow.insertCell();
+          newCell.onclick = changeColor;
+        }
+    }
+  }
+
+  // change the color of the clicked cell to current color
+  function changeColor() {
+    this.style.background = $colorPicker.value;
+  }
+
+})();
